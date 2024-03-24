a796532e118e110978e454caa1bd1913659d311f
Владислав <elektronvladklochan@gmail.com>
2024-03-24 18:50:27 +0700

    Create WinForm App

---
+++ DiscreteMath-Lab1/Model/Classes/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteMath_Lab1.Model.Classes
+{
+    internal class Vertex
+    {
+    }
+}
